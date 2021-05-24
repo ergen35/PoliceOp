1fba8842673de5b5860d391788f2b44442fcd170
AveryMkv3 <ienoverse@gmail.com>
2021-05-24 12:05:46 +0100

    Commit

---
+++ PoliceOp.Models/Session.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoliceOp.Models
+{
+    class Session
+    {
+        public int ID { get; set; }
+        public Guid SessionID { get; set; }
+    }
+}
